91b2ec4eace3e62c65ef98c7fc1e4c52df0847d8
JG-PR01 <jgglobal12@gmail.com>
2022-08-09 06:06:47 -0400

    Final del Proyecto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const navigation = [
 	{ name: 'Proyectos', href: '#proyectos', current: false },
@@ -16,7 +16,7 @@ const Navbar = () => {
 	return (
 		<Disclosure as="nav" className="bg-indigo-700 fixed w-full z-20">
 			{({ open }) => (
-				<>
+				<Fragment>
 					<div className="max-w-7xl mx-auto px-2 py-2 sm:px-6 lg:px-8">
 						<div className="relative flex items-center justify-between h-16">
 							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -90,7 +90,7 @@ const Navbar = () => {
 							))}
 						</div>
 					</Disclosure.Panel>
-				</>
+				</Fragment>
 			)}
 		</Disclosure>
 	);
