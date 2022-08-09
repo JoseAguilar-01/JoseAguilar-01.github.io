d32c29931720bffcf91efd99b5c6b4f45593f8d5
JG-PR01 <jgglobal12@gmail.com>
2022-08-08 22:20:31 -0400

    Agregando directorio src

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Contacto from '../components/Contacto';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import ListadoProyectos from '../components/ListadoProyectos';
+import ListadoHabilidades from '../components/ListadoHabilidades';
+import Navbar from '../components/Navbar';
+import Particle from '../components/Particle';
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<Particle />
+
+			<Navbar />
+
+			<Header />
+
+			<div className="bg-white separador-superior__1 mt-24"></div>
+
+			<ListadoProyectos />
+
+			<ListadoHabilidades />
+
+			<div className="bg-white separador-inferior__1"></div>
+
+			<Contacto />
+
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
