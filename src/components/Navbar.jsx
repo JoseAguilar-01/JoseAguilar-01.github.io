ce2b756dc778e680ce4f5085088b144569a164fc
JG-PR01 <jgglobal12@gmail.com>
2022-08-09 06:22:40 -0400

    Final del Proyecto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Fragment } from 'react';
 import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
@@ -16,7 +17,7 @@ const Navbar = () => {
 	return (
 		<Disclosure as="nav" className="bg-indigo-700 fixed w-full z-20">
 			{({ open }) => (
-				<Fragment>
+				<div>
 					<div className="max-w-7xl mx-auto px-2 py-2 sm:px-6 lg:px-8">
 						<div className="relative flex items-center justify-between h-16">
 							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -90,7 +91,7 @@ const Navbar = () => {
 							))}
 						</div>
 					</Disclosure.Panel>
-				</Fragment>
+				</div>
 			)}
 		</Disclosure>
 	);
